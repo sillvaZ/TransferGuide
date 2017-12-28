8259fed18a39f94e3abe27b8d22b6a8fd46ddcdc
sillva <ota@sillva-mac.local>
2017-12-29 00:59:44 +0900

    init

---
+++ TransferGuide/Model/Font.swift
@@ -0,0 +1,10 @@
+//
+//  Font.swift
+//  TransferGuide
+//
+//  Created by sillva on 2017/12/29.
+//  Copyright © 2017年 sillva. All rights reserved.
+//
+
+struct Font {
+}
---
+++ TransferGuide/View/ViewController/DateConditionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DateConditionViewController.swift
+//  TransferGuide
+//
+//  Created by sillva on 2017/12/29.
+//  Copyright © 2017年 sillva. All rights reserved.
+//
+
+import Foundation
